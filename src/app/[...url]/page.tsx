f537a73c019e1d75eef02c4399a0e244d7e5c3e5
GabrielMCC2020 <gabrielmcc2002cultural@gmail.com>
2024-09-10 10:25:04 -0500

    first commit

---
+++ src/app/[...url]/page.tsx
@@ -0,0 +1,12 @@
+interface PageProps {
+  params: {
+    url: string | string[] | undefined;
+  };
+}
+
+const Page = ({ params }: PageProps) => {
+  console.log(params);
+  return <p>hello</p>;
+};
+
+export default Page;
